6c4efba588902d2afde58ea6b7706725eaccab6b
Kyriakos Barbounakis <k.barbounakis@gmail.com>
2023-02-11 05:07:50 +0000

    update @themost/client

--- examples/app/src/index.js
+++ examples/app/src/index.js
@@ -1,6 +1,35 @@
 const { getApplication, serveApplication } = require('@themost/test');
 const path = require('path');
 const express = require('express');
+const { Router } = require('express');
+
+function insertRouterBefore(parent, before, insert) {
+    const beforeIndex = parent.stack.findIndex( (item) => {
+        return item === before;
+    });
+    const findIndex = parent.stack.findIndex( (item) => {
+        return item === insert;
+    });
+    // remove last router
+    parent.stack.splice(findIndex, 1);
+    // move up
+    parent.stack.splice(beforeIndex, 0, insert);
+}
+
 const app = getApplication();
 app.use('/home', express.static('public/home'));
+
+const addRouter = Router()
+addRouter.use('^/$', (req, res,next) => {
+    return res.redirect('auth/login?redirect_uri=/home')
+});
+app.use('/', addRouter);
+
+const router = app._router;
+const before = router.stack.find((item) => {
+    return item.name === 'router' && item.regexp && item.regexp.test('/');
+});
+const insert = router.stack[router.stack.length - 1];
+insertRouterBefore(router, before, insert)
+
 serveApplication(app, 8080);
